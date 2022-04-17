0aebe8d4683c0baee95909d5141758e68eefbe57
moscowsky <moscowskyad@gmail.com>
2022-04-17 12:36:23 +0300

    bb matching do smth

---
+++ src/bb_matching.cpp
@@ -0,0 +1,82 @@
+/*
+ * Launching bb_matching algorithm many times on random generated data 
+ */
+
+#include <stdio.h>
+#include "geometry_utils.h"
+#include <ctime>
+#include <chrono>
+
+int W = 620;
+int H = 480;
+
+using namespace cv;
+
+eod::ExtendedObjectInfo generate_rect(){
+    int x1 = std::rand() % W;
+    int x2 = std::rand() % W;
+    int y1 = std::rand() % H;
+    int y2 = std::rand() % H;
+    
+    int x = min(x1,x2);
+    int y = min(y1,y2);
+    
+    int h = max(x1,x2) - x;
+    int w = max(y1,y2) - y;
+    
+    return eod::ExtendedObjectInfo(x,y,w,h);
+    
+}
+
+void generate_rects(int N, std::vector <eod::ExtendedObjectInfo> &rects){
+    rects.clear();
+    for( int i = 0 ; i < N ; i++){
+        rects.push_back(generate_rect());
+    }    
+}
+
+int main(int argc, char **argv){
+    if( argc < 2){
+        printf("Wrong usage!\nUsage: ./bb_matching min_rects max_rects results\nExample: ./bb_matching 10 100 ../../data/bb_matching_100");
+    }    
+    int min_rects = std::atoi(argv[1]);
+    int max_rects = std::atoi(argv[2]);    
+    int iou_threshold_d = 0.75;
+    printf("Calculation started");
+    
+    std::srand(std::time(nullptr));
+    
+    std::vector<eod::ExtendedObjectInfo> rectsA;
+    std::vector<eod::ExtendedObjectInfo> rectsB;
+    for( int i = min_rects ; i < max_rects ; i++){
+        for( int j = min_rects ; j < max_rects ; j++){
+            generate_rects(i, rectsA);
+            generate_rects(j, rectsB);
+            
+            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();            
+            Mat_<double> closenessMapD = createClosenessMap(&rectsA, &rectsB, iou_threshold_d);
+            Mat mask(closenessMapD.size(), CV_8UC1, Scalar(255,255,255));            
+            //objects.clear();
+            while( true ){
+                double min, max = 0;
+                Point min_loc, max_loc;
+                minMaxLoc(closenessMapD, &min, &max, &min_loc, &max_loc, mask);
+                if( max == 0 ) break;                               
+                    
+                eod::ExtendedObjectInfo newone;                
+                newone = rectsA[max_loc.y] & rectsB[max_loc.x];   
+                                
+                //objects.push_back(newone); 
+                mask.row(max_loc.y).setTo(Scalar(0,0,0));                                    
+                mask.col(max_loc.x).setTo(Scalar(0,0,0));                                
+            }
+            mask.release();
+            closenessMapD.release();
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            double elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count();
+            printf("[%i, %i]: %f ms\n",i,j,elapsed_time);
+            
+        }
+    }
+    
+}
